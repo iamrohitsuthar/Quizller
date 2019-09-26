1ef1343cad621b6daa5e7a13c0b3a2ea304b9d65
iamrohitsuthar <iamrohitsuthar@gmail.com>
2019-09-26 17:45:41 +0530

    add database connection configuration

---
+++ database/config.php
@@ -0,0 +1,8 @@
+<?php
+//database configurations
+define("DB_HOST","localhost");
+define("DB_UNAME","root");
+define("DB_PASS","root");
+define("DB_DNAME","quizller");
+$conn=mysqli_connect(DB_HOST,DB_UNAME,DB_PASS,DB_DNAME);
+?>
