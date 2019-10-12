89523e996aa75133e6fceeac051e34825d01dbdc
iamrohitsuthar <iamrohitsuthar@gmail.com>
2019-10-12 16:28:41 +0530

    add corerct count and wrong count update in check answer and add LIKE in question answer check

--- files/check_answer.php
+++ files/check_answer.php
@@ -18,16 +18,23 @@
     if (!$conn) 
         die("Connection failed: " . mysqli_connect_error());
     else{
-        $result = mysqli_query($conn, "SELECT id FROM Questions WHERE id = '".$question_id."' AND correctAns = '".$selected_option."' LIMIT 1 ");        
+        $result = mysqli_query($conn, "SELECT id FROM Questions WHERE id = '".$question_id."' AND correctAns LIKE '%{$selected_option}%' LIMIT 1 ");        
         if (mysqli_num_rows($result) > 0){
+            //increase question corerct count
+            $sql = "UPDATE score set correct_count = correct_count + 1 where question_id = '$question_id'";
+            mysqli_query($conn,$sql);
            if(mysqli_query($conn, "UPDATE students set score = score + '".$score_earned."' where id = '".$student_id."' ")){
                 echo "SCORE_UPDATED_SUCCESSFULLY";
            }else{
                 echo "SCORE_UPDATE_FAILURE";
             }  
         }
-        else
+        else {
+            //increase question wrong count
+            $sql = "UPDATE score set wrong_count = wrong_count + 1 where question_id = '$question_id'";
+            mysqli_query($conn,$sql);
             echo "WRONG_ANSWER";
+        }
     }
 
     mysqli_close($conn);
