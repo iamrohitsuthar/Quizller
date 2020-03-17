4afc89d30b0bd3386b726b4447bb2e46480e1e9c
Rohit Suthar <iamrohitsuthar@gmail.com>
2020-03-17 09:54:31 +0530

    Update config.php

--- database/config.php
+++ database/config.php
@@ -1,8 +1,8 @@
 <?php
 //database configurations
 define("DB_HOST","localhost");
-define("DB_UNAME","id10946898_quizller");
-define("DB_PASS","quizller");
-define("DB_DNAME","id10946898_quizller");
+define("DB_UNAME","YOUR_DATABASE_USER_NAME");
+define("DB_PASS","YOUR_DATABASE_PASSWORD");
+define("DB_DNAME","YOUR_DATABASE_NAME");
 $conn=mysqli_connect(DB_HOST,DB_UNAME,DB_PASS,DB_DNAME);
 ?>
